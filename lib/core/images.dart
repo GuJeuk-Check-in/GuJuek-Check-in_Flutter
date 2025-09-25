5cd0d285efb8c735811e61fd4e8abd804e9a972d
taejun <ptj01036251@gmail.com>
2025-09-25 21:09:36 +0900

    feat: 이미지 리소스 경로 관리 클래스 추가

---
+++ lib/core/images.dart
@@ -0,0 +1,20 @@
+final String baseSafetyRules = 'assets/images/safety_rules';
+final String baseLogo = 'assets/images/logo';
+
+class Images {
+  Images_.();
+
+  //logo
+  static String guLogo = '$baseLogo/';
+
+  //safety_rules
+  static String accident = '$baseSafetyRules/accident.png';
+  static String clean = '$baseSafetyRules/clean.png';
+  static String fire = '$baseSafetyRules/fire.png';
+  static String kind = '$baseSafetyRules/kind.png';
+  static String listen = '$baseSafetyRules/listen.png';
+  static String sick = '$baseSafetyRules/sick.png';
+  static String stair = '$baseSafetyRules/stair.png';
+  static String talk = '$baseSafetyRules/talk.png';
+  static String trash = '$baseSafetyRules/trash.png';
+}
