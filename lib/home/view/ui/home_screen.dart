df99a754791376b72c79e4d65448b8489ee40d67
taejun <ptj01036251@gmail.com>
2025-10-13 17:14:21 +0900

    feat: 홈 화면 퍼블리싱 완료

---
+++ lib/home/view/ui/home_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gujuek_check_in_flutter/component/circle_background.dart';
+import 'package:gujuek_check_in_flutter/component/custom_layout.dart';
+import 'package:gujuek_check_in_flutter/core/images.dart';
+import 'package:gujuek_check_in_flutter/home/view/widgets/custom_elevated_button.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomLayout(
+        child: Stack(
+      children: [
+        const CircleBackground(),
+        Column(
+          children: [
+            SizedBox(height: 80.h),
+            Center(
+              child: Image.asset(
+                Images.guLogo,
+                width: 758.w,
+                height: 290.h,
+              ),
+            ),
+            SizedBox(height: 50.h),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CustomElevatedButton(
+                  text: '시설 이용 신청',
+                  textStyle: TextStyle(
+                    color: Colors.white,
+                    fontSize: 48.sp,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  onPressed: () {},
+                  backgroundColor: const Color(0xff3AB9FF),
+                ),
+                SizedBox(width: 108.w),
+                CustomElevatedButton(
+                  text: '회원가입',
+                  textStyle: TextStyle(
+                    color: Colors.white,
+                    fontSize: 48.sp,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  onPressed: () {},
+                  backgroundColor: const Color(0xff6C39AE),
+                ),
+              ],
+            )
+          ],
+        ),
+      ],
+    ));
+  }
+}
