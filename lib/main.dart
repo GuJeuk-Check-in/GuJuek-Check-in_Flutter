ef207488013fcace8ab739d1fb80293ed5be62f5
taejun <ptj01036251@gmail.com>
2025-10-13 13:17:39 +0900

    refactor: 폰트 Pretendard로 변경

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
-          fontFamily: 'Inter',
+          fontFamily: 'Pretendard',
         ),
         home: const HomeScreen(),
       ),
