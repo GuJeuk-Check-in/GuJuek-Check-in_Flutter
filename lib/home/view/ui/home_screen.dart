4ee49fcd7d890922571fe43ae82db97e55a235d2
taejun <ptj01036251@gmail.com>
2025-10-14 21:54:11 +0900

    feat: 시설 이용 신청 다이얼로그창 퍼블리싱

--- lib/home/view/ui/home_screen.dart
+++ lib/home/view/ui/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:gujuek_check_in_flutter/component/circle_background.dart';
 import 'package:gujuek_check_in_flutter/component/custom_layout.dart';
 import 'package:gujuek_check_in_flutter/core/images.dart';
 import 'package:gujuek_check_in_flutter/home/view/widgets/custom_elevated_button.dart';
+import 'package:gujuek_check_in_flutter/home/view/widgets/facility_registration_dialog.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -11,49 +12,56 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CustomLayout(
-        child: Stack(
-      children: [
-        const CircleBackground(),
-        Column(
-          children: [
-            SizedBox(height: 80.h),
-            Center(
-              child: Image.asset(
-                Images.guLogo,
-                width: 758.w,
-                height: 290.h,
+      child: Stack(
+        children: [
+          const CircleBackground(),
+          Column(
+            children: [
+              SizedBox(height: 80.h),
+              Center(
+                child: Image.asset(
+                  Images.guLogo,
+                  width: 758.w,
+                  height: 290.h,
+                ),
               ),
-            ),
-            SizedBox(height: 50.h),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CustomElevatedButton(
-                  text: '시설 이용 신청',
-                  textStyle: TextStyle(
-                    color: Colors.white,
-                    fontSize: 48.sp,
-                    fontWeight: FontWeight.w700,
+              SizedBox(height: 50.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CustomElevatedButton(
+                    text: '시설 이용 신청',
+                    textStyle: TextStyle(
+                      color: Colors.white,
+                      fontSize: 48.sp,
+                      fontWeight: FontWeight.w700,
+                    ),
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (_) {
+                            return const FacilityRegistrationDialog();
+                          });
+                    },
+                    backgroundColor: const Color(0xff3AB9FF),
                   ),
-                  onPressed: () {},
-                  backgroundColor: const Color(0xff3AB9FF),
-                ),
-                SizedBox(width: 108.w),
-                CustomElevatedButton(
-                  text: '회원가입',
-                  textStyle: TextStyle(
-                    color: Colors.white,
-                    fontSize: 48.sp,
-                    fontWeight: FontWeight.w700,
+                  SizedBox(width: 108.w),
+                  CustomElevatedButton(
+                    text: '회원가입',
+                    textStyle: TextStyle(
+                      color: Colors.white,
+                      fontSize: 48.sp,
+                      fontWeight: FontWeight.w700,
+                    ),
+                    onPressed: () {},
+                    backgroundColor: const Color(0xff6C39AE),
                   ),
-                  onPressed: () {},
-                  backgroundColor: const Color(0xff6C39AE),
-                ),
-              ],
-            )
-          ],
-        ),
-      ],
-    ));
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
