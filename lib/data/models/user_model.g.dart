cfd909cac201559e5eb86c35783e0cd9310ad5f0
taejun <ptj01036251@gmail.com>
2025-10-17 18:51:06 +0900

    feat: 데이터 모델링

---
+++ lib/data/models/user_model.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UserModel _$UserModelFromJson(Map<String, dynamic> json) => UserModel(
+  name: json['name'] as String,
+  gender: $enumDecode(_$GenderEnumMap, json['gender']),
+  phone: json['phone'] as String,
+  birthYMD: json['birthYMD'] as String,
+  privacyAgreed: json['privacyAgreed'] as bool,
+  purpose: (json['purpose'] as List<dynamic>).map((e) => e as String).toList(),
+);
+
+Map<String, dynamic> _$UserModelToJson(UserModel instance) => <String, dynamic>{
+  'name': instance.name,
+  'gender': _$GenderEnumMap[instance.gender]!,
+  'phone': instance.phone,
+  'birthYMD': instance.birthYMD,
+  'privacyAgreed': instance.privacyAgreed,
+  'purpose': instance.purpose,
+};
+
+const _$GenderEnumMap = {Gender.Man: 'Man', Gender.Woman: 'Woman'};
