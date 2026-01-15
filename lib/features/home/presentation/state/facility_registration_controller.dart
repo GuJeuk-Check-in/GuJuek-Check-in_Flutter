c348496698ef87e1149a7febc91de1e9f7564ee7
taejun <ptj01036251@gmail.com>
2026-01-15 12:16:23 +0900

    refactor: 폴더 위치 및 파일 경로 변경

--- lib/features/home/presentation/state/facility_registration_controller.dart
+++ lib/features/home/presentation/state/facility_registration_controller.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gujuek_check_in_flutter/data/models/login/login_model.dart';
-import 'package:gujuek_check_in_flutter/data/repositories/auth_repository.dart';
 
+import '../../../auth/domain/repositories/auth_repository.dart';
 import 'facility_registration_state.dart';
 
 class FacilityRegistrationController
